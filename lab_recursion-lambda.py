669c987962defbf3519bee1d77e628612a047d0f
hess-01 <hessah-14@hotmail.com>
2023-05-12 13:09:17 +0300

    conditanl solved

--- lab_recursion-lambda.py
+++ lab_recursion-lambda.py
@@ -1,27 +1,25 @@
 
 # Using recursion, if given a word/phrase return how many vowels(a,e,i,o,u) 
 # are in that phrase or word:
-def count_vowels(word):
- 
-#base case:
 
- if the word is empty,
- return 0 
- if len(word) ==0:
-  return 0
- 
- #recursive case : countthe vowels in the rest of the word and add 1 if the first letter is vowel
- rest_count = count_vowels(word[1:])
- if word[0] in "aeiouAEIOU":
-   return rest_count+1
- else:
-  return rest_count
- 
- #to use this function, simply call it with a string argument 
+vowels = "aeiou"
 
- word = "Hello, World!"
- num_vowels = count_vowels(word)
- print("ther are", num_vowels, "vowels in", word)
+def count_vowels(pharse:str) ->int:
+
+ if pharse == "":
+   return 0 
+ 
+ count = 0 
+ if pharse[0].lower() in vowels:
+    count = 1
+
+    return 1 + count_vowels(pharse[1:])
+  
+    vowels_count = count_vowels ("I love python")
+    print (vowels_count)
+ 
+
+ 
 
 
 
