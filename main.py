e4518b46486c9ff3f7975f663bc5a507c5aa1dcf
SulimanBinM <sbinmohammed_1@icloud.com>
2023-05-31 21:18:26 +0300

    Done.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 print("---------- Q1 ----------")
-def vowelsCounter(word:str) -> int :
-    vowels = ["a","e","i","o","u"]
-    count = 0 
-    for char in word:
-        if char in vowels:
-            count+=1
-    return count
+vowels = ["a","e","i","o","u"]
+count = 0 
+count_vol = 0 
+
+def vowelsCounter(word:str):
+    if word=="":
+        return 0
+    if word[-1].lower() in vowels:
+        tot_vol= 1 + vowelsCounter(word[:len(word)-1])
+        return tot_vol
+    else:
+        decrmentWord=vowelsCounter(word[:len(word)-1])
+        return decrmentWord
+
 userIput = input("Enter a phrase: ").lower()
 print(f"the number of the vowels : \n{vowelsCounter(userIput)}")
 print("---------- Q2 ----------")
